81e171589b6d5a778bce17793e0bf1a270945e61
Виталий <vetla1495@gmail.com>
2016-09-07 17:46:31 +0300

    Service management

--- app/actions/loadimage.php
+++ app/actions/loadimage.php
@@ -1,10 +1,14 @@
 <?php
-
+$connection = pg_connect("host=195.88.209.17 port=5432 dbname=makeup user=postgres password=12345_Vet");
+$uploadDate = date("dmy") . date("His");
 $uploaddir = '/var/www/html/storage/photos/';
-$uploadfile = $uploaddir . basename($_FILES['file1']['name']);
+$uploadfilename = $uploadDate . basename($_FILES['file1']['name']);
+$uploadfile = $uploaddir . $uploadDate . basename($_FILES['file1']['name']);
+$email = $_POST['email'];
 
 echo '<pre>';
 if (move_uploaded_file($_FILES['file1']['tmp_name'], $uploadfile)) {
+    $result = pg_query($connection, "UPDATE profiles SET photo='$uploadfilename' WHERE email='$email'");
     echo "Файл корректен и был успешно загружен.\n";
 } else {
     echo "Возможная атака с помощью файловой загрузки!\n";
