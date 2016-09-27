118f061f2e84a2af3970ee71bb691e1625c74353
Виталий <vetla1495@gmail.com>
2016-09-28 01:46:24 +0300

    to notebook

--- others/pizzatt/addorder.php
+++ others/pizzatt/addorder.php
@@ -2,8 +2,9 @@
 $orderId = $_GET['id'];
 $orderTime = date("dmy");
 $connection = pg_connect("host=195.88.209.17 port=5432 dbname=pizzatt user=postgres password=12345_Vet");
-$get = pg_query($connection, "SELECT order_id FROM orders WHERE order_time = '$orderTime'");
-$query = pg_query($connection, "INSERT INTO orders VALUES ($orderId,'" . $orderTime . "')");
+$get = pg_query($connection, "SELECT order_num FROM orders ORDER BY order_num DESC");
+$nextNum = pg_fetch_array($get);
+$query = pg_query($connection, "INSERT INTO orders VALUES ($orderId,'" . $orderTime . "', $nextNum[0]+1)");
 if ($query) {
     print "[{\"result\":\"success\"}]";
 } else {
